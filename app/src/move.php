9731d66717c2cdc77e399ab3165e079cbcd0c05e
DPijlman <dannypijlman@gmail.com>
2024-07-08 22:16:18 +0200

    Green phase grasshopper
    Implemented grasshopper feature and added unit test for this class

--- app/src/move.php
+++ app/src/move.php
@@ -4,4 +4,3 @@ require_once __DIR__ . '/vendor/autoload.php';
 use HiveGame\MoveHandler\MoveHandler;
 
 MoveHandler::move($_POST['from'], $_POST['to']);
-?>
--- app/src/tests/grasshopperTest.php
+++ app/src/tests/grasshopperTest.php
@@ -2,14 +2,55 @@
 use PHPUnit\Framework\TestCase;
 use HiveGame\Features\Grasshopper;
 
-class GrasshopperTest extends TestCase
-{
-    public function testGrasshopperMove()
-    {
+class GrasshopperTest extends TestCase {
+    public function testGrasshopperValidMove() {
         // Arrange
+        $board = [
+            '0,0' => [[0, 'queen']],
+            '1,0' => [[1, 'queen']],
+            '2,0' => [[0, 'grasshopper']]
+        ];
+        $from = '2,0';
+        $to = '-1,0'; // Changed the destination to an empty space
 
         // Act
+        $result = Grasshopper::isValidMove($board, $from, $to);
 
         // Assert
+        $this->assertTrue($result);
+    }
+
+    public function testGrasshopperInvalidMoveEmptySpace() {
+        // Arrange
+        $board = [
+            '0,0' => [[0, 'queen']],
+            '2,0' => [[0, 'grasshopper']]
+        ];
+        $from = '2,0';
+        $to = '0,0';
+
+        // Act
+        $result = Grasshopper::isValidMove($board, $from, $to);
+
+        // Assert
+        $this->assertFalse($result);
+    }
+
+    public function testGrasshopperInvalidMoveOccupiedSpace() {
+        // Arrange
+        $board = [
+            '0,0' => [[0, 'queen']],
+            '1,0' => [[1, 'queen']],
+            '2,0' => [[0, 'grasshopper']],
+            '3,0' => [[0, 'ant']]
+        ];
+        $from = '2,0';
+        $to = '3,0';
+
+        // Act
+        $result = Grasshopper::isValidMove($board, $from, $to);
+
+        // Assert
+        $this->assertFalse($result);
     }
 }
