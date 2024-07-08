f2898c3b0bf81b56b122d10d59ea35a899c305e7
DPijlman <dannypijlman@gmail.com>
2024-07-08 22:21:00 +0200

    Red phase soldier ant
    This commit starts the red phase of the implementation of the soldier ant class

---
+++ app/src/tests/soldierAntTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use HiveGame\Features\soldiertAnt;
+
+class soldierAntTest extends TestCase {
+    public function testGrasshopperValidMove() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
